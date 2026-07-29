8ff317fd9a4c15ca1b18221481c04f080a440b1b
xvorost <xvorostpython@gmail.com>
2026-07-29 16:11:55 +0300

    feat: add auth login service

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
+from app.schemas.auth import LoginRequest, TokenResponse
 from app.schemas.user import UserCreate, UserRead
 
-__all__ = ["UserCreate", "UserRead"]
+__all__ = ["LoginRequest", "TokenResponse", "UserCreate", "UserRead"]
--- app/services/auth.py
+++ app/services/auth.py
@@ -1,9 +1,9 @@
 from fastapi import HTTPException, status
 
-from app.core.security import get_password_hash
+from app.core.security import create_access_token, get_password_hash, verify_password
 from app.models import User
 from app.repositories import UserRepository
-from app.schemas import UserCreate
+from app.schemas import LoginRequest, UserCreate
 
 
 class AuthService:
@@ -30,3 +30,16 @@ class AuthService:
             username=user_create.username,
             hashed_password=hashed_password,
         )
+
+    def login_user(self, login_request: LoginRequest) -> str:
+        user = self.user_repository.get_by_email(login_request.email)
+
+        if not user or not verify_password(
+            login_request.password, user.hashed_password
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid email or password",
+            )
+
+        return create_access_token(subject=str(user.id))
